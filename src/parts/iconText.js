58f31310a7f318a7e52326a4a4f9774784ea1a76
HelianaCh <helianachristianti@gmail.com>
2021-03-30 22:17:14 +0700

    Initial commit

---
+++ src/parts/iconText.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from 'elements/Button'
+import IconBrand from "assets/images/icons/icon-brand.svg"
+
+export default function iconText() {
+    return (
+        <Button className="brand-text-icon" href = "" type="link">
+           <img  
+                            width ="96" 
+                            height="96"
+                            src={IconBrand} 
+                            />
+                            
+            {/* <Button className="brand-text-icon" href = "" type="link">
+            Comfort<span className="text-yellow">Zone</span>
+            
+        </Button> */}
+        </Button>
+    );
+}
